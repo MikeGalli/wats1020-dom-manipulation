d693c8e0dc757e42ae28a6f823c71a281b89e5b0
MikeGalli <mikegalligar@mikes-mbp-2.seattleu.edu>
2016-04-23 17:14:36 -0700

    fed

--- js/main.js
+++ js/main.js
@@ -27,6 +27,7 @@ $( document ).ready(function() {
     $('#login-form .btn').on('click', function(event) {
       $('#login-form').hide();
       $('.user-info').show();
+      $('.user-fullname').val="ZZZ";
     });
 
     // TODO: Create a function to listen for clicks on all the "View Details"
@@ -38,16 +39,42 @@ $( document ).ready(function() {
     //      3. Toggle visibility of all the elements within that parent with the class `details`.
     //      4. Change the text of the "view details" button to read "hide details" so the user
     //          understands they can hide the text again.
-    $('.view-details').on('click', function(event){
-        console.log(event);
-          var targetElement = event.target;
-          var container = targetElement.parentElement.parentElement;
-          $(container).find('.details').each(function(index, el){
-              if ($(el).is(':visible')){
-                  $(el).fadeOut();
-                  targetElement.innerText = "View Details"
 
-              } else {
+/*  VDB NOTES ON: VIEW DETAILS BUTTONS: 1 - 12:
+    1) Get stuff in all the classes called “view-details”.
+    2) When something in that class gets clicked; create an object called event.
+    3) var targetElement = event.target - the element that triggered the event.
+
+    4) var container = targetElement.parentElement.parentElement //
+      Find the “grandparent of, event.target. In this case targetElement is the button
+      that got clicked. find the container of the container of THAT button and…….
+    5) parentElement - JQ object, returns the parent element of specified element.
+
+    6) In the grandparent of the button that got clicked, find an element that has the class details
+    7) .each The function inside each will return a list of the things that were found.
+      The list will be an array. It will contain index & value pairs. In this case, there is only 1 thing to find.
+      the value of what is found is represented by, el. The whole point is to toggle el between visible & invisible.
+    8) is - A JQ method.
+    9) ':visible' - A JQ psudeo selector. = select things that are visible.
+        = opacity != 0
+        = display != hidden
+        = height & width != 0
+    10) If the targetElement is visible, fadeOut.
+    11) After making the text disapear: Change the text of the button to say: "View Details".
+    12) If el != visible, fadeIn and set the button to read: "Hide Details".
+
+*/
+                                                                    /*VDB NOTES*/
+    $('.view-details').on('click', function(event){                 /*1-2*/
+        console.log(event);
+          var targetElement = event.target;                         /*3*/
+          var container = targetElement.parentElement.parentElement;/*4-5*/
+          $(container).find('.details').each(function(index, el){   /*6-7*/
+              if ($(el).is(':visible')){                            /*8-9*/
+                  $(el).fadeOut();                                  /*10*/
+                  targetElement.innerText = "View Details"          /*11*/
+
+              } else {                                              /*12*/
                   $(el).fadeIn();
                   targetElement.innerText = "Hide Details"
               }
