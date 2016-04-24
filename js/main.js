54ba42edcaa1a60239ce166b641dc08dff09ccd0
MikeGalli <mikegalligar@android-e0d47d7280a24e47.seattleu.edu>
2016-04-24 15:03:40 -0700

    sdfb

--- js/main.js
+++ js/main.js
@@ -97,8 +97,8 @@ $( document ).ready(function() {
     $('.greatest-progress').css('width', greatestPercent);                    /*8*/
     console.log(greatPercent);
     console.log(greatestPercent);
-    $('.boxGreat').html("Number of votes for Great are: " + voteCounts.great);
-    $('.boxGreatest').html("Number of votes for Greatest of all time are: " + voteCounts.greatest);
-    $('.boxTotal').html("The total number of votes is: " + voteCounts.total);
+    $('.boxGreat').html("Number of votes for Great are: " + voteCounts.great + "   ");
+    $('.boxGreatest').html("Number of votes for Greatest of all time are: " + voteCounts.greatest + "   ");
+    $('.boxTotal').html("The total number of votes is: " + voteCounts.total + "   ");
     });
 });
